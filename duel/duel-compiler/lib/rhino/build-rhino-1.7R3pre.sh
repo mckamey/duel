9a1fcda878b64bf9c9a05018c8fedf8a7c46bac8
mckamey <devnull@localhost>
2011-04-23 08:57:01 -0700

    - updating to Rhino 1.7R3 release

--- duel/duel-compiler/lib/rhino/build-rhino-1.7R3pre.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cvs -d ':pserver:anonymous@cvs-mirror.mozilla.org:/cvsroot' co mozilla/js/rhino rhino
-cd rhino
-ant jar
-mv -f build/rhino1_7R3pre/js.jar ../js-1.7R3pre.jar
--- duel/duel-compiler/lib/rhino/install-rhino-1.7R3pre.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mvn install:install-file -Dfile=js-1.7R3pre.jar -DgroupId=rhino -DartifactId=js -Dversion=1.7R3 -Dpackaging=jar
