fda8644afa93d6ee6ad760179fef856fb7425196
mckamey <devnull@localhost>
2011-04-23 20:23:38 -0700

    updating to v0.2.2 beginning to test out deploy settings

--- duel/duel-compiler/lib/rhino/install-rhino-1.7R3.sh
+++ duel/duel-compiler/lib/rhino/install-rhino-1.7R3.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-mvn install:install-file -Dfile=js.jar -DgroupId=rhino -DartifactId=js -Dversion=1.7R3 -Dpackaging=jar
+
+mvn install:install-file \
+-Dfile=js.jar \
+-DgroupId=rhino \
+-DartifactId=js \
+-Dversion=1.7R3 \
+-Dpackaging=jar
+
+mvn deploy:deploy-file \
+-Dversion=1.7R3 \
+-DartifactId=js \
+-DgroupId=rhino \
+-DgeneratePom=true \
+-Dpackaging=jar \
+-Dfile=duel-compiler/lib/rhino/js.jar \
+-Durl=file:///duel-mvn-repo-hg \
+-DrepositoryId=mvn.duelengine.org.local
