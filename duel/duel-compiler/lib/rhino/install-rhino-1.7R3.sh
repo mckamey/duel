ad3cdb07b3d54ea580541755ce58a711c1c188cc
mckamey <devnull@localhost>
2011-07-10 07:17:51 -0700

    simplifying maven coordinates before first deployment to maven central repository

--- duel/duel-compiler/lib/rhino/install-rhino-1.7R3.sh
+++ duel/duel-compiler/lib/rhino/install-rhino-1.7R3.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
 mvn install:install-file \
--Dfile=js.jar \
--DgroupId=rhino \
--DartifactId=js \
--Dversion=1.7R3 \
--Dpackaging=jar
-
-mvn deploy:deploy-file \
--Dversion=1.7R3 \
--DartifactId=js \
--DgroupId=rhino \
--DgeneratePom=true \
--Dpackaging=jar \
--Dfile=duel-compiler/lib/rhino/js.jar \
--Durl=file:///duel-mvn-repo-hg \
--DrepositoryId=mvn.duelengine.org.local
+	-DgroupId=org.mozilla \
+	-DartifactId=rhino \
+	-Dversion=1.7R3 \
+	-Dpackaging=jar \
+	-Dfile=js.jar
