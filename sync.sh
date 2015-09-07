0f23d417d31a0b49f1c5a2a4eead680c12dcd3aa
mckamey <devnull@localhost>
2015-09-06 23:41:12 -0700

    Updating s3cmd settings.

--- sync.sh
+++ sync.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-# exclude '.DS_Store' and build files
+clear;clear;
+
+BUCKET='s3://duel/'
+DISTRO='cf://E3G86IU7BFHQIK'
+SOURCE='./duel-js/target/'
+
+# exclude '.DS_Store' and hg files
+
 s3cmd sync \
+	--add-header='Vary: Accept-Encoding' \
+	--no-mime-magic \
+	--guess-mime-type \
+	--encoding=UTF-8 \
+	--add-encoding-exts=foo \
+	--reduced-redundancy \
 	--cf-invalidate \
 	--acl-public \
 	--recursive \
@@ -10,7 +23,14 @@ s3cmd sync \
 	--exclude 'duel.js' \
 	--exclude 'duel-render.js' \
 	--exclude 'duel-dom.js' \
-	./duel-js/target/ \
-	s3://duel/
+	--exclude '.git/*' \
+	--exclude '.gitignore' \
+	--exclude '.hg/*' \
+	--exclude '.hgignore' \
+	--exclude '.hgtags' \
+	--exclude '*.sh' \
+	--exclude 'tomcat/*' \
+	${SOURCE} \
+	${BUCKET}
 
-# s3cmd cfinvalinfo cf://E3G86IU7BFHQIK
+#s3cmd cfinvalinfo ${DISTRO}
